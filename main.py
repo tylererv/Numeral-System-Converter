54bf7094f4b08734960019e1574bfbcb8d1d2d47
tylererv <tylerervin193176@yahoo.com>
2024-09-09 13:29:21 -0400

    Updated fromBinary and fromOctal to accept decimal point conversions.

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-def fromBinary(biNum: int) -> int: #Converts Binary to Decimal.
-    decNum: int = 0
+def fromBinary(biNum: str) -> float: #Converts Binary to Decimal.
+    decNum: float = 0.0
     lenBin = len(str(biNum))
 
-    for i in range(lenBin):
-        decNum += int(str(abs(biNum))[i]) * 2 ** (lenBin - 1 - i)
+    if '.' in biNum:
+        decimalPoint = biNum.find('.')
+        intPart = biNum[:decimalPoint]
+        fractionPart = biNum[decimalPoint + 1:]
+
+        for i in range(len(intPart)):
+            decNum += int(intPart[i]) * 2 ** (len(intPart) - 1 - i)
+
+        for i in range(len(fractionPart)):
+            decNum += int(fractionPart[i]) * (1 / (2 ** (i + 1)))
+
+    else:
+        for i in range(lenBin):
+            decNum += int(biNum[i]) * 2 ** (lenBin - 1 - i)
     return decNum
 
-def fromOctal(octNum: int) -> int: #Converts Octal to Decimal.
-    decNum: int = 0
-    lenOct = len(str(octNum))
+def fromOctal(OcNum: str) -> float: #Converts Octal to Decimal.
+    decNum: float = 0
+    lenOct = len(str(OcNum))
 
-    for i in range(lenOct):
-        decNum += int(str(abs(octNum))[i]) * 8 ** (lenOct - 1 - i)
+    if '.' in OcNum:
+        decimalPoint = OcNum.find('.')
+        intPart = OcNum[:decimalPoint]
+        fractionPart = OcNum[decimalPoint + 1:]
+
+        for i in range(len(intPart)):
+            decNum += int(intPart[i]) * 8 ** (len(intPart) - 1 - i)
+
+        for i in range(len(fractionPart)):
+            decNum += int(fractionPart[i]) * (1 / (8 ** (i + 1)))
+
+    else:
+        for i in range(lenOct):
+            decNum += int(OcNum[i]) * 8 ** (lenOct - 1 - i)
     return decNum
+
 
 def fromHex(hexNum: str) -> int: #Converts Hexadecimal to Decimal.
     decNum: int = 0
@@ -29,5 +54,3 @@ def fromHex(hexNum: str) -> int: #Converts Hexadecimal to Decimal.
         else:
             decNum += int(hexNum[i]) * 16 ** (len(hexNum) - 1 - i)
     return decNum
-# Yo
-# 1

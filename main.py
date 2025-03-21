19c9061444c3b0a061d3cd1c2eb775eceac53a35
tylererv <tylerervin193176@yahoo.com>
2025-03-21 10:29:34 -0400

    fixed some issues

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import time
 
+
 def check(number: str, numeralType: str) -> bool:  # Checks the validity of a binary, octal, or hex number
     validNums = []
 
-    if numeralType.lower() == "binary": # then
+    if numeralType.lower() == "binary":  # then
         validNums = ["0", "1", "."]
     elif numeralType.lower() == "octal":
         validNums = ["0", "1", "2", "3", "4", "5", "6", "7", "."]
     elif numeralType.lower() == "hex" or numeralType.lower() == "hexadecimal":
         validNums = ["0", "1", "2", "3", "4", "5", "6", "7",
                      "8", "9", "A", "B", "C", "D", "E", "F", "."]
+    elif numeralType.lower() == "decimal":
+        validNums = ["0", "1", "2", "3", "4", "5", "6", "7",
+                     "8", "9"]
     number = number.upper()
 
     for i in number:
         if i not in validNums:
             return False
     return True
+
 
 def fromBinaryToDecimal(biNum: str) -> float:  # Converts Binary to Decimal.
     if check(biNum, "Binary"):
@@ -38,8 +43,9 @@ def fromBinaryToDecimal(biNum: str) -> float:  # Converts Binary to Decimal.
                 decNum += int(biNum[i]) * 2 ** (lenBin - 1 - i)
         return decNum
     else:
-        print("Try a valid Binary Number")
+        print("Try a valid Binary Number.\n")
         return -1
+
 
 def fromOctalToDecimal(OcNum: str) -> float:  # Converts Octal to Decimal.
     if check(OcNum, "Octal"):
@@ -62,8 +68,9 @@ def fromOctalToDecimal(OcNum: str) -> float:  # Converts Octal to Decimal.
                 decNum += int(OcNum[i]) * 8 ** (lenOct - 1 - i)
         return decNum
     else:
-        print("Try a valid Octal number.")
+        print("Try a valid Octal number.\n")
         return -1
+
 
 def fromHexToDecimal(hexNum: str) -> int:  # Converts Hexadecimal to Decimal.
     if check(hexNum, "Hex"):
@@ -83,42 +90,74 @@ def fromHexToDecimal(hexNum: str) -> int:  # Converts Hexadecimal to Decimal.
                 decNum += int(hexNum[i]) * 16 ** (len(hexNum) - 1 - i)
         return decNum
     else:
-        print("Try a valid Hex number.")
+        print("Try a valid Hex number.\n")
         return -1
 
-def run() -> None: # Allows the user to pick what Numeral System will be converted and input a number to be converted
-    toBeConverted: str = input("What Numeral System would you like to convert to Decimal? ")
 
-    match toBeConverted.lower():
+def fromDecimalToBinary(decNum: str) -> int:  # converts a decimal number into a binary number
+    # add the ability to do negative numbers later
+    if check(decNum, "Decimal"):
+        binNum: str = ""
+        decNumInt = int(decNum)
 
-        case "binary":
-            num: str = input("Input the Binary number: ")
-            convertedNum = fromBinaryToDecimal(num)
-            if convertedNum != -1:
-                print(f'{num} in Binary is {convertedNum} in Decimal.')
+        if '.' in decNum:
+            print("Not done yet bud")
+            return -1
+        else:
+            while decNumInt >= 1:
+                binNum = str(decNumInt % 2) + binNum
+                decNumInt = decNumInt // 2
+        return int(binNum)
+    else:
+        print("Try a valid Decimal number.\n")
+        return -1
 
-        case "octal":
-            num: str = input("Input the Octal number: ")
-            convertedNum = fromOctalToDecimal(num)
-            if convertedNum != -1:
-                print(f'{num} in Octal is {convertedNum} in Decimal.')
 
-        case "hex":
-            num: str = input("Input the Hexadecimal number: ")
-            convertedNum = fromHexToDecimal(num)
-            if convertedNum != -1:
-                print(f'{num} in Hexadecimal is {convertedNum} in Decimal.')
+def run() -> None:  # Allows the user to pick what Numeral System will be converted and input a number to be converted
+    keepGoing = True
+    while keepGoing:
+        toBeConverted: str = input("What Numeral System would you like to convert \nor type break to end process: ")
+        # convertedInto: str = input(f"What Numeral System would you like to convert {toBeConverted} into: ")
 
-        case "hexadecimal":
-            num: str = input("Input the Hexadecimal number: ")
-            convertedNum = fromHexToDecimal(num)
-            if convertedNum != -1:
-                print(f'{num} in Hexadecimal is {convertedNum} in Decimal.')
+        match toBeConverted.lower():
 
-        case _:
-            print("Invalid Numeral System Try Agian.\n")
-            time.sleep(.5)
-            run()
+            case "binary":
+                num: str = input("Input the Binary number: ")
+                convertedNum = fromBinaryToDecimal(num)
+                time.sleep(.3)
+                if convertedNum != -1:
+                    print(f'{num} in Binary is {convertedNum} in Decimal.\n')
+
+            case "octal":
+                num: str = input("Input the Octal number: ")
+                convertedNum = fromOctalToDecimal(num)
+                time.sleep(.3)
+                if convertedNum != -1:
+                    print(f'{num} in Octal is {convertedNum} in Decimal.\n')
+
+            case "hex":
+                num: str = input("Input the Hexadecimal number: ")
+                convertedNum = fromHexToDecimal(num)
+                time.sleep(.3)
+                if convertedNum != -1:
+                    print(f'{num} in Hexadecimal is {convertedNum} in Decimal.\n')
+
+            case "hexadecimal":
+                num: str = input("Input the Hexadecimal number: ")
+                convertedNum = fromHexToDecimal(num)
+                time.sleep(.3)
+                if convertedNum != -1:
+                    print(f'{num} in Hexadecimal is {convertedNum} in Decimal.\n')
+
+            case "break":
+                break
+
+            case _:
+                print("Invalid Numeral System(s) Try Agian.\n")
+                time.sleep(.5)
+                run()
+
 
 if __name__ == '__main__':
-   run()
+    run()
+
